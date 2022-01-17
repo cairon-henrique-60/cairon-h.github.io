b2b2c3a01e95cb1085c791b1e46087d1e94bd671
Emilia Pacheco <liapacheco777@gmail.com>
2022-01-17 18:42:41 -0300

    Formulario Fornecedores

---
+++ CadastroFornecedor/CadastroFornecedor/main.js
@@ -0,0 +1,17 @@
+window.onload=function(){
+
+var btn = document.getElementById('btn-action');
+
+
+var container = document.querySelector('.container');
+btn.addEventListener('click', function() {
+    
+    if(container.style.display === 'block') {
+        container.style.display = 'none';
+    } else {
+        container.style.display = 'block';
+    }
+})
+
+}
+
